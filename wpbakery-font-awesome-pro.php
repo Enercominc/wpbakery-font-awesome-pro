75070cd59ebfbb16a4819adb34781a932a7a18be
Bradford Knowlton <brad@velpeo.com>
2021-05-23 11:44:11 -0700

    initial commit

---
+++ wpbakery-font-awesome-pro.php
@@ -0,0 +1,14 @@
+/**
+ * Plugin Name:       WPBakery Font Awesome Pro 🧁
+ * Plugin URI:        https://github.com/Velpeo/wpbakery-font-awesome-pro
+ * Description:       Handle the additional code needed to go pro
+ * Version:           4.1.7
+ * Requires at least: 5.4
+ * Requires PHP:      7.2
+ * Author:            Bradford Knowlton
+ * Author URI:        https://bradknowlton.com/
+ * License:           BSD 3-clause
+ * License URI:       https://opensource.org/licenses/BSD-3-Clause
+ * Text Domain:       velpeo
+ * Domain Path:       /languages
+ */
